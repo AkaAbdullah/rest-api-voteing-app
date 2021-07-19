7a36037d8f5b6143143060c4ab10f674464c8629
Attari Ak <akaabdullah00@gmail.com>
2021-07-19 19:12:40 +0500

    first commit

---
+++ DRF/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/posts/', views.PostList.as_view()),
+    path('api/posts/<int:pk>/vote', views.VoteCreate.as_view())
+]
